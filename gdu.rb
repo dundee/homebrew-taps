0cd3be347282dd5a93a79ae0623073410968fc2a
Daniel Milde <daniel.milde@firma.seznam.cz>
2021-01-10 22:22:49 +0100

    gdu 2.2.0

--- gdu.rb
+++ gdu.rb
@@ -1,14 +1,14 @@
 class Gdu < Formula
   desc "Disk usage analyzer with console interface written in Go"
   homepage "https://github.com/dundee/gdu"
-  url "https://github.com/dundee/gdu/archive/v1.6.0.tar.gz"
-  sha256 "f4cb8b7450b8996b80983087dfde65528a9862fe1f412c345d66556149d79623"
+  url "https://github.com/dundee/gdu/archive/v2.2.0.tar.gz"
+  sha256 "94f4faaee6c1676b73bb75f031cc3d004b8df713fb12d1dfc90ed592bf302df7"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags=\"-s -w -X 'main.AppVersion=v1.6.0'\"", "."
+    system "go", "build", "-ldflags=\"-s -w -X 'main.AppVersion=v2.2.0'\"", "."
     bin.install "gdu" => "gdu"
   end
 
