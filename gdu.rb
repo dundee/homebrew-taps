c51e7e6d428327b335e7f7a9e7b2956be784cfcc
Daniel Milde <daniel.milde@firma.seznam.cz>
2021-01-10 23:18:13 +0100

    use std_go_args, extract version

--- gdu.rb
+++ gdu.rb
@@ -1,15 +1,15 @@
 class Gdu < Formula
   desc "Disk usage analyzer with console interface written in Go"
   homepage "https://github.com/dundee/gdu"
-  url "https://github.com/dundee/gdu/archive/v2.2.0.tar.gz"
+  version "2.2.0"
+  url "https://github.com/dundee/gdu/archive/v#{version}.tar.gz"
   sha256 "94f4faaee6c1676b73bb75f031cc3d004b8df713fb12d1dfc90ed592bf302df7"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags=\"-s -w -X 'main.AppVersion=v2.2.0'\"", "."
-    bin.install "gdu" => "gdu"
+    system "go", "build", *std_go_args, "-ldflags=\"-s -w -X 'main.AppVersion=v#{version}'\"", "."
   end
 
   test do
